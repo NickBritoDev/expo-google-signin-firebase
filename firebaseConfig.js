97cc6809ee9b4ad04c0d6e711958816a74518b32
Brito <101473345+NickBritoDev@users.noreply.github.com>
2024-02-06 12:42:23 -0300

    test ubuntu

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBBqokzeuy0_HwfxB-HP1voYq7jvb08znk",
@@ -12,6 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth }
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth };
